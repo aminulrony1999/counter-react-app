8ba7b5483a7a73681af610b502c2f5ac24428379
aminulrony1999 <aminulrony1999@gmail.com>
2023-09-30 10:19:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
+}
+function Counter(){
+  const abc = useState(0);
+  console.log(abc);
+  return(
+    <div>
+      <h1>Count:</h1>
+      <button>Increase</button>
+    </div>
+  )
 }
 
 export default App;
